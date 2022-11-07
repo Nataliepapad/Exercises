73a16b94f54c4743a8da5ece98cfa80bd933d230
Natalia Papadatou <papadnatalia@gmail.com>
2022-11-07 17:35:51 +0200

    Update

--- src/ShouldWakeUp.java
+++ src/ShouldWakeUp.java
@@ -1,8 +1,8 @@
 public class ShouldWakeUp {
     public static boolean shouldWakeUp(boolean barking, int hourOfDay ) {
-        if (barking == false || hourOfDay < 0 || hourOfDay > 23){
+        if ( !barking || (hourOfDay < 0 || hourOfDay > 23)){
             return false;
-        } else if (barking == true && (hourOfDay < 8 || hourOfDay > 22)){
+        } else if (barking && (hourOfDay < 8 || hourOfDay > 22)){
             return true;
         }
         return false;
